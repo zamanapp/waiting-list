4c1c1bc394e018a5722912ed32427a9237aadcda
gimy <b.afatah@yahoo.com>
2023-05-12 03:51:17 +0800

    feat: initial start

---
+++ components/common/DefaultFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="fixed bottom-0 w-full text-center p-3">
+    <!-- add copyrights -->
+    <div>
+      Copyright &copy; {{ new Date().getFullYear() }} {{ appConfig.appName }}
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig();
+</script>
---
+++ components/common/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="flex justify-between mx-6 items-center">
+    <NuxtLink class="text-5xl" to="/">*</NuxtLink>
+    <div class="flex gap-3">
+      <button class="capitalize">{{ mode }}</button>
+      <button>English</button>
+      <button>Waitlist</button>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { useColorMode } from "@vueuse/core";
+
+const mode = useColorMode(); // Ref<'dark' | 'light'>
+</script>
