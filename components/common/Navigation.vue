e6ca6302a8d105cdf5fdb66d05173e06bb0b4469
gimyboya <b.afatah@yahoo.com>
2024-05-26 22:05:09 +0800

    feat: added logo

--- components/common/Navigation.vue
+++ components/common/Navigation.vue
@@ -2,8 +2,18 @@
   <nav
     class="absolute grid items-center justify-between w-screen grid-cols-5 px-12 py-3 backdrop-blur-xl"
   >
-    <NuxtLink class="col-span-2 w-28" to="/">
+    <NuxtLink
+      class="inline-flex items-center col-span-2 gap-2 align-middle"
+      to="/"
+    >
       <Logo class="h-9 w-9 dark:fill-gray-200" />
+      <div class="pb-1 text-3xl leading-loose">
+        <span class="font-bold">z</span>
+        <span class="font-semibold">a</span>
+        <span class="font-medium">m</span>
+        <span>a</span>
+        <span class="font-light">n</span>
+      </div>
     </NuxtLink>
 
     <div class="flex justify-center">
