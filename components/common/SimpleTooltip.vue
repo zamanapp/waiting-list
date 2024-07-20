2e60c875ebc73eebc84b9c8ff10d4cf3a78f6e87
gimy <b.afatah@yahoo.com>
2024-07-20 10:50:01 +0800

    feat: add tooltips for social icons

---
+++ components/common/SimpleTooltip.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+defineProps({
+  content: String,
+});
+</script>
+
+<template>
+  <TooltipProvider :delay-duration="200">
+    <Tooltip>
+      <TooltipTrigger>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{{ content }}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
--- components/common/SocialIcons.vue
+++ components/common/SocialIcons.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="flex items-center gap-3">
-    <NuxtLink to="https://t.me/zamanapp" target="__blank">
-      <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:telegram" />
-    </NuxtLink>
-    <NuxtLink to="https://x.com/zaman_hq">
-      <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:new-twitter" />
-    </NuxtLink>
-    <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:linkedin-01" />
-    <NuxtLink to="https://github.com/zamanapp" target="__blank">
-      <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:github" />
-    </NuxtLink>
+    <SimpleTooltip content="Telegram">
+      <NuxtLink to="https://t.me/zamanapp" target="__blank">
+        <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:telegram" />
+      </NuxtLink>
+    </SimpleTooltip>
+    <SimpleTooltip content="Twitter | X">
+      <NuxtLink to="https://x.com/zaman_hq" target="__blank">
+        <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:new-twitter" />
+      </NuxtLink>
+    </SimpleTooltip>
+    <SimpleTooltip content="Instagram">
+      <NuxtLink to="https://www.linkedin.com/company/zamanhq" target="__blank">
+        <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:linkedin-01" />
+      </NuxtLink>
+    </SimpleTooltip>
+    <SimpleTooltip content="Github">
+      <NuxtLink to="https://github.com/zamanapp" target="__blank">
+        <Icon class="w-5 h-5 cursor-pointer" icon="hugeicons:github" />
+      </NuxtLink>
+    </SimpleTooltip>
   </div>
 </template>
 
