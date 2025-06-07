7157fbcc6c97259849b1e8545bba92f039f2fcdf
khawarizmus <b.afatah@yahoo.com>
2025-06-07 20:17:20 +0800

    fix: update Wird component styling and image positioning for improved layout and visual consistency

--- components/differentiators/Wird.vue
+++ components/differentiators/Wird.vue
@@ -2,16 +2,16 @@
   <GlareLineFrame
     title="Your Wird"
     description="Schedule and track your memorization, revision, and recitation of the Quran"
-    class="flex p-px lg:col-span-3"
+    class="relative flex p-px lg:col-span-3"
+    hide-overflow
   >
     <div
-      class="relative overflow-hidden [mask-composite:intersect] [mask-image:linear-gradient(to_left,transparent,black_27rem),linear-gradient(to_top,transparent,black_27rem)]"
+      class="relative overflow-hidden [mask-composite:intersect] [mask-image:linear-gradient(to_left,transparent,black_7rem),linear-gradient(to_top,transparent,black_7rem)]"
     >
       <QuranLogo class="fill-emerald-600" :width="100" :height="100" />
+      <img src="/img/wird.png" alt="Wird" class="absolute bottom-12 start-16" />
     </div>
-    <div>
-      <img src="/img/wird.png" alt="Wird" class="w-full h-full" />
-    </div>
+
     <!-- <QuranMemorization /> -->
   </GlareLineFrame>
 </template>
